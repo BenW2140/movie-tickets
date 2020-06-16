628501482dd2780421dd2eda06aa8352da14480f
Ben White <bwhite2140@outlook.com>
2020-06-16 14:05:39 -0700

    add business logic for ticket object and select and text forms in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+function Ticket() {
+  this.title = title,
+  this.time = time,
+  this.age = age
+}
+
+Ticket.prototype.calculatePrice = function (ticket) {
+  let price = 10;
+  if (ticket.title === "Avatar 2") {
+    price += 2;
+  }
+  if (ticket.age >= 55 || ticket.age < 13) {
+    price--;
+  }
+  if (ticket.time === "Morning") {
+    price--;
+  }
+}
+
 $(document).ready(function() {
 
 });
